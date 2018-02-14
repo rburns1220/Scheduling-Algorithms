d27bc4e251d3bd3fb5df8ca746ab07cf70b75348
rburns1220 <36008383+rburns1220@users.noreply.github.com>
2018-02-13 20:03:33 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "pcb.h"
 #include "data.h"
 #include "queue.h"
-//#include "algorithm.h"
+#include "sjf.h"
 
 int main()
 {
+    FILE* out;
+    Data* data;
 
+    out = fopen("process.out", "w");
+    data = parseFile("process.in");
+
+    if (strcmp(data->algorithm, "sjf") == 0)
+        shortestJobFirst(data, out);
+    // Add other algorithms here *****
+
+    destroyData(data);
+    fclose(out);
+    return 0;
 }
